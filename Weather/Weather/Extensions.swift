f506cf67c1d326a5dca2b0867feb2938996ecde7
Linus Fackler <78768883+linusfackler@users.noreply.github.com>
2023-03-02 12:42:05 -0600

    update

---
+++ Weather/Weather/Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Extensions.swift
+//  Weather
+//
+//  Created by Linus Fackler on 3/2/23.
+//
+
+import Foundation
+
+extension Double {
+    func roundDouble() -> String {
+        return String(format: "%.0f", self)
+    }
+}
---
+++ Weather/Weather/Views/WeatherView.swift
@@ -0,0 +1,55 @@
+//
+//  WeatherView.swift
+//  Weather
+//
+//  Created by Linus Fackler on 3/2/23.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var weather: ResponseBody
+    var body: some View {
+        ZStack(alignment: .leading) {
+            VStack {
+                VStack(alignment: .leading, spacing: 5) {
+                    Text(weather.name)
+                        .bold().font(.title)
+                    
+                    Text("Today, \(Date().formatted(.dateTime.month().day().hour().minute()))")
+                    fontWeight(.light)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Spacer()
+                
+                VStack {
+                    HStack {
+                        VStack(spacing: 20) {
+//                            Image(systemName:)
+                        }
+                        .frame(width: 150, alignment: .leading)
+                        
+                        Text(weather.main.feelsLike.roundDouble() + "º")
+                            .font(.system(size: 100))
+                            .fontWeight(.bold)
+                            .padding()
+                    }
+                    
+                }
+                .frame(maxWidth: .infinity)
+            }
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .leading)
+        }
+        .edgesIgnoringSafeArea(.bottom)
+        .background(Color(hue: 0.539, saturation: 1.0, brightness: 1.0))
+        .preferredColorScheme(.dark)
+    }
+}
+
+struct WeatherView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherView(weather: previewWeather)
+    }
+}
